253e0041ce79fd1ff6586c70a8f6e3c2950a7694
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-03-11 00:53:55 -0400

    Arreglo del Contexto

--- Angel_Morel_P2_AP1/DAL/Contexto.cs
+++ Angel_Morel_P2_AP1/DAL/Contexto.cs
@@ -1,6 +1,43 @@
-﻿namespace Angel_Morel_P2_AP1.DAL
+﻿
+using Angel_Morel_P2_AP1.Model;
+using Angel_Morel_P2_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Angel_Morel_P2_AP1.DAL;
+
+public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
 {
-    public class Contexto
+    public virtual DbSet<Encuestas> Encuestas { get; set; }
+    public virtual DbSet<Ciudades> Ciudades { get; set; }
+
+    public virtual DbSet<EncuestaDestalles> Destalles { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Ciudades>().HasData(
+            new List<Ciudades>()
+            {
+            new()
+            {
+                CiudadId = 1,
+                Nombre = "Nagua",
+                Monto = 0,
+
+            },
+            new()
+            {
+                CiudadId = 2,
+                Nombre = "Samana",
+                Monto = 0,
+            },
+            new()
+            {
+                CiudadId = 3,
+                Nombre = "Rio San Juan",
+                Monto = 0,
+            }
+
+            }
+        );
+        base.OnModelCreating(modelBuilder);
     }
 }
