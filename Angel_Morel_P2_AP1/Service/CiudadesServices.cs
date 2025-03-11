309c28bca06684cfa546c196140d09b18da9df78
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-03-11 00:56:56 -0400

    Creacion de la clase CiudadesServices en el Service

---
+++ Angel_Morel_P2_AP1/Service/CiudadesServices.cs
@@ -0,0 +1,21 @@
+﻿using Angel_Morel_P2_AP1.DAL;
+using Angel_Morel_P2_AP1.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Angel_Morel_P2_AP1.Service
+{
+    public class CiudadesServices
+    {
+        public object DbFactory { get; private set; }
+
+        public async Task<List<Ciudades>> Listar(Expression<Func<Ciudades, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Ciudades
+                .Where(criterio)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
