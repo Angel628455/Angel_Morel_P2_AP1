7c0cf064359499503e69e12ce1c2ea19a79f0d91
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-03-11 00:55:01 -0400

    Creacion de la clase Ciudades

---
+++ Angel_Morel_P2_AP1/Model/Ciudades.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Angel_Morel_P2_AP1.Model
+{
+    public class Ciudades : DbContext
+    {
+        public Ciudades()
+        {
+        }
+
+        public Ciudades(DbContextOptions<Ciudades> options) : base(options)
+        {
+        }
+
+        public DbSet<CiudadDetalle> CiudadDetalles { get; set; }
+        public int CiudadId { get; internal set; }
+        public string Nombre { get; internal set; }
+        public int Monto { get; internal set; }
+    }
+}
