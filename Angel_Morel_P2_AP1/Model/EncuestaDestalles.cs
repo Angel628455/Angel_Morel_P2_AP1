93c9d8c29386b076055e6aae45f9bf160f87cd5a
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-03-11 00:55:24 -0400

    Creacion de la clase Encuesta de ciudades

---
+++ Angel_Morel_P2_AP1/Model/EncuestaDestalles.cs
@@ -0,0 +1,20 @@
+﻿using Angel_Morel_P2_AP1.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Angel_Morel_P2_AP1.Models;
+
+public class EncuestaDestalles
+{
+    [Key]
+    public int DestallesId { get; set; }
+
+    public int EncuestaId { get; set; }
+
+    public int CiudadId { get; set; }
+
+    public int MontoEncuesta { get; set; }
+
+    [ForeignKey("CiudadId")]
+    public virtual Ciudades Ciudad { get; set; } = null!;
+}
