60a72529788f4a3f3ec36a987db391194cc4b001
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-03-11 00:54:39 -0400

    Creacion de la clase Ciudad detalle

---
+++ Angel_Morel_P2_AP1/Model/CiudadDetalle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Angel_Morel_P2_AP1.Model
+{
+    public class CiudadDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+
+        public decimal Monto { get; set; }
+
+        public int NombreId { get; set; }
+
+        public int AsignaturaId { get; set; }
+
+        public DateTime Fecha { get; set; }
+    }
+}
