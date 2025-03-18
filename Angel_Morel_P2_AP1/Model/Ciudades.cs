95d0871c80e877d727485b720a96ca1973f6ab38
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-03-17 20:27:30 -0400

    Correcion de errores

--- Angel_Morel_P2_AP1/Model/Ciudades.cs
+++ Angel_Morel_P2_AP1/Model/Ciudades.cs
@@ -1,20 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Angel_Morel_P2_AP1.Model
+namespace Angel_Morel_P2_AP1.Model;
+
+public class Ciudades
 {
-    public class Ciudades : DbContext
-    {
-        public Ciudades()
-        {
-        }
-
-        public Ciudades(DbContextOptions<Ciudades> options) : base(options)
-        {
-        }
-
-        public DbSet<CiudadDetalle> CiudadDetalles { get; set; }
-        public int CiudadId { get; internal set; }
-        public string Nombre { get; internal set; }
-        public int Monto { get; internal set; }
-    }
+    [Key]
+    public int CiudadId { get; set; }  
+    public string Nombre { get; set; } = string.Empty;
+    public int Monto { get; set; }
 }
